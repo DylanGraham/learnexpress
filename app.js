2c12ff3daefbf5cd7df987ee1303f8c838ada5c7
Dylan Graham <dylan.graham@gmail.com>
2017-02-20 16:28:50 +1100

    hello

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var app = express();
 var port = 8000;
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+app.get('/', function (req, res) {
+    res.send('Hello world!\n');
+});
+app.get('/books', function (req, res) {
+    res.send('Hello books!\n');
+});
 app.listen(port, function (err) {
     console.log('Running server on port ' + port);
 });
--- app.ts
+++ app.ts
@@ -2,6 +2,17 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+app.get('/', (req, res) => {
+    res.send('Hello world!\n');
+});
+
+app.get('/books', (req, res) => {
+    res.send('Hello books!\n');
+});
+
 app.listen(port, err => {
     console.log('Running server on port ' + port);
 });
