eb4996c23dc92aab5d33a0dc1aab4d44a90a558d
Dylan Graham <dylan.graham@gmail.com>
2017-02-22 13:25:23 +1100

    Inject

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,25 @@
 const gulp = require('gulp');
+
 gulp.task('inject', () => {
     const wiredep = require('wiredep').stream;
-    const options = {
+    const inject = require('gulp-inject');
+    const injectSrc = gulp.src([
+        './public/css/*.css',
+        './public/js/*.js'],
+        {read: false}
+    );
+    const injectOptions = {
+        ignorePath: '/public/'
+    };
+
+    const wiredepOptions = {
         bowerJson: require('./bower.json'),
         directory: './public/lib',
         ignorePath: '../../public/'
     };
 
     return gulp.src('./src/views/*.html')
-        .pipe(wiredep(options))
+        .pipe(wiredep(wiredepOptions))
+        .pipe(inject(injectSrc, injectOptions))
         .pipe(gulp.dest('./src/views'));
 });
