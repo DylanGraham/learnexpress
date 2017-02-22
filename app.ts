1194e126ac0d2380b3a27bb50cf6faa66741e881
Dylan Graham <dylan.graham@gmail.com>
2017-02-22 14:35:15 +1100

    EJS

--- app.ts
+++ app.ts
@@ -3,10 +3,11 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 app.use(express.static('public'));
-app.use(express.static('src/views'));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.send('Hello world!\n');
+    res.render('index', {title: 'Hello from render', list: ['a', 'b', 'c']});
 });
 
 app.get('/books', (req, res) => {
